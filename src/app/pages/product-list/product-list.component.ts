2f6e82534d463a66ab75d86c741f1b9bfaecf46b
Wan Muzaffar <wanmuz86@gmail.com>
2024-11-13 15:29:19 +0700

    call the api

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -4,6 +4,9 @@ import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ProductHttpService } from '../../services/product-http.service';
+import { lastValueFrom } from 'rxjs';
+
 @Component({
   selector: 'app-product-list',
   standalone: true,
@@ -13,16 +16,45 @@ import { RouterModule } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
 
-  products:Product[] = []
+  products:any[] = []
 // Inject
-  constructor(private productService:ProductService){}
+  constructor(
+   // private productService:ProductService
+   private productHttpService:ProductHttpService
+    ){}
 
 
   ngOnInit(): void {
+
     // When the page is loaded , I will retrieve the product from the service
     // To use the service, I will inject it here
-    this.products = this.productService.getAllProducts();
+
+    //Without API
+ //   this.products = this.productService.getAllProducts();
+
+
+ /// API Call
+ // When working with Asynchronous function, eg: API call, Ex working with file, ex local storage
+ // We are working with Observable
+ // Is a data type that has 3 output -> Successful, Failure, or Pending
+ // One of tHe way to get data from observable is using async await
+ 
+  this.loadProducts()
+  
+  }
+  // Marked this function as async since I am calling the API from here (Working with Observable) 
+  async loadProducts(){
+
+    try {
+// Wait for the asyncronouse process to finish
+// After it finish, put the value inside this.products
+    this.products = await lastValueFrom(this.productHttpService.getProducts());
+    
+  } catch(e){
+    console.log(e)
+  }
 
   }
 
 }
+
--- src/app/services/product-http.service.ts
+++ src/app/services/product-http.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductHttpService {
 
-  constructor() { }
+  // Import and Inject httpClient (for API Call)
+  private productUrl = "https://fakestoreapi.com/products"
+
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts(): Observable<any>{
+    return this.httpClient.get(this.productUrl);
+  }
+
+  addProduct(name:string, price:number){
+    const data = {"name":name, "price":price};
+    return this.httpClient.post(this.productUrl,data);
+  }
 }
