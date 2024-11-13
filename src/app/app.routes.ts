b4936ea6d8112a89ad41580ce181baf5c3c77647
Wan Muzaffar <wanmuz86@gmail.com>
2024-11-13 13:09:28 +0700

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { AddProductComponent } from './pages/add-product/add-product.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'products/:id',
+        component:ProductsComponent
+    },
+    {
+        path:'add-product',
+        component:AddProductComponent
+    },
+    {
+        path:'',
+        component:ProductListComponent
+    },
+    {
+        path:'**',
+        component:NotFoundComponent
+    }
+];
