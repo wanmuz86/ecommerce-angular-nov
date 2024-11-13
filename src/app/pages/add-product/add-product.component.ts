cf5cad68bf94d774cf9b070ebedf9cdd7297e2d0
Wan Muzaffar <wanmuz86@gmail.com>
2024-11-13 14:46:44 +0700

    add product in add product page

--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
 export class AddProductComponent {
 
+  constructor(private productService:ProductService, private router:Router){}
+  product : Product = {
+    id:0,
+    name:'',
+    price:0
+  }
+
+  addProduct(){
+    this.productService.addProduct(this.product.name , this.product.price);
+    this.router.navigate(['/']);
+  }
 }
