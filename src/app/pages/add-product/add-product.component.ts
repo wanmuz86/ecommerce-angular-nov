070917b415770f3815e76c63bbd30568968fa0e5
Wan Muzaffar <wanmuz86@gmail.com>
2024-11-13 15:40:28 +0700

    add example for post item

--- src/app/pages/add-product/add-product.component.ts
+++ src/app/pages/add-product/add-product.component.ts
@@ -3,6 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { Product } from '../../models/product';
 import { ProductService } from '../../services/product.service';
 import { Router } from '@angular/router';
+import { ProductHttpService } from '../../services/product-http.service';
+import { lastValueFrom } from 'rxjs';
+
 @Component({
   selector: 'app-add-product',
   standalone: true,
@@ -12,15 +15,30 @@ import { Router } from '@angular/router';
 })
 export class AddProductComponent {
 
-  constructor(private productService:ProductService, private router:Router){}
+  constructor(
+    //private productService:ProductService, 
+    private productHttpService:ProductHttpService,
+    private router:Router
+    
+    ){}
   product : Product = {
     id:0,
     name:'',
     price:0
   }
 
-  addProduct(){
-    this.productService.addProduct(this.product.name , this.product.price);
+  async addProduct(){
+   // this.productService.addProduct(this.product.name , this.product.price);
+   try {
+    const result = await lastValueFrom(this.productHttpService.addProduct(this.product.name, this.product.price))
+    console.log(result)
     this.router.navigate(['/']);
+   }
+   catch (e){
+    console.log(e)
+
+   }
+  
+   
   }
 }
